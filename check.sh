f069ed519043b9bf9ca8ece9f95dd10d3e8d4d36
Miroslava Ivanova <miroslava.ivanova@progress.com>
2019-11-05 10:40:48 +0200

    new yml

---
+++ check.sh
@@ -0,0 +1,7 @@
+    tns run ios 1>out.txt 2>err.txt &
+    grep -q 'Successfully synced application' <(tail -f out.txt)
+    's$taps$clicks$g' app/main-view-model.js
+    cat app/main-view-model.js
+    cat out.txt
+    grep R "Successfully sync" "out.txt"
+   
